a30aac6f13587fff9dd4f321a98186fcf3e19c07
romidas99 <tkaihsien619@yahoo.com>
2025-10-26 16:06:39 +0800

    Fix checkstyle

--- src/test/java/seedu/address/model/company/ApplicationFilterPredicateTest.java
+++ src/test/java/seedu/address/model/company/ApplicationFilterPredicateTest.java
@@ -4,9 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
 
 import seedu.address.testutil.PersonBuilder;
 
